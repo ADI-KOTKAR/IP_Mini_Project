b89a076dffff6edd0d0f52eca8fe61884242bd42
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-12-12 00:09:24 +0530

    :sparkles: Landing Page added

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>TechVents | Home</title>
+</head>
+
+<body>
+    <div class="container">
+        <header class="head my-3">
+            <nav class="navbar navbar-expand-lg navbar-light head__custom-nav">
+                <a class="navbar-brand d-flex align-items-center" href="#">
+                    <img src="images/icons8-bluestacks-75.png" alt="website logo">
+                    <span>
+                        <img src="images/Techvents-text-removebg-preview.png" alt="">
+                    </span>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                    <span><a class="nav-link" href="#" style="text-decoration: none; color: black;">Login</a></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="Blog.php">Blogs</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Login.php">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    </div>
+    <div class="container">
+        <div class="row custom-section d-flex align-items-center">
+            <div class="col-12 col-lg-4">
+                <h2>Tech<img src="https://img.icons8.com/color/48/000000/plus--v2.png"/></h2>
+                <h3>Events</h3>
+                <h5>Blogs | Events | News</h5>
+                <p>A <b> One-Stop Destination</b> for Quality Tech related stuff.</p>
+                <a href="#">Get Started</a>
+            </div>
+            <div class="col-12 col-lg-8">
+                <img src="images/img1.png" alt="Team process banner">
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
