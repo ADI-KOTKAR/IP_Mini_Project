1fadbd1740d6e125af98179a2512238246bee439
shreyaskhadapkar <shreyaskhadapkar@gmail.com>
2020-12-11 21:12:04 +0530

    Home Page UI Done

--- Home.php
+++ Home.php
@@ -9,5 +9,136 @@
 </head>
 <body>
     <?php include("base.php"); ?>
+    <div class="body-container">
+        <div class="post-listing">
+        <!-- For loop idhar aayega -->
+            <div class="post-container">
+                <img src="./uploads/AI.jpg" alt="">
+                <div class="post-text">
+                    <div class="first-line">
+                        <h3 class="post-title"><a href="FullPost.php"> AIII For AlLLLLLllaL </a></h3>
+                        <p class="read-time">6 mins</p>
+                    </div>
+                    <div class="second-line">
+                        <p class="post-descp">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Perspiciatis fugiat minima dolore nulla sequi. Architecto a eveniet omnis eligendi, amet quaerat ad maiores ipsam eius placeat maxime animi. 
+                            Repellendus blanditiis minus facere ullam, adipisci enim nostrum rerum nam facilis nobis esse placeat veniam sint delectus quasi quibusdam vero. 
+                            
+                        </p>
+                    </div>
+                    <div class="third-line">
+                        <img src="https://img.icons8.com/ios/25/000000/applause.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="post-container">
+                <img src="./uploads/AI.jpg" alt="">
+                <div class="post-text">
+                    <div class="first-line">
+                        <h3 class="post-title"><a href="FullPost.php"> AIII For AlLLLLLllaL </a></h3>
+                        <p class="read-time">6 mins</p>
+                    </div>
+                    <div class="second-line">
+                        <p class="post-descp">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Perspiciatis fugiat minima dolore nulla sequi. Architecto a eveniet omnis eligendi, amet quaerat ad maiores ipsam eius placeat maxime animi. 
+                            Repellendus blanditiis minus facere ullam, adipisci enim nostrum rerum nam facilis nobis esse placeat veniam sint delectus quasi quibusdam vero. 
+                            
+                        </p>
+                    </div>
+                    <div class="third-line">
+                        <img src="https://img.icons8.com/ios/25/000000/applause.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="post-container">
+                <img src="./uploads/AI.jpg" alt="">
+                <div class="post-text">
+                    <div class="first-line">
+                        <h3 class="post-title"><a href="FullPost.php"> AIII For AlLLLLLllaL </a></h3>
+                        <p class="read-time">6 mins</p>
+                    </div>
+                    <div class="second-line">
+                        <p class="post-descp">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Perspiciatis fugiat minima dolore nulla sequi. Architecto a eveniet omnis eligendi, amet quaerat ad maiores ipsam eius placeat maxime animi. 
+                            Repellendus blanditiis minus facere ullam, adipisci enim nostrum rerum nam facilis nobis esse placeat veniam sint delectus quasi quibusdam vero. 
+                            
+                        </p>
+                    </div>
+                    <div class="third-line">
+                        <img src="https://img.icons8.com/ios/25/000000/applause.png" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="post-container">
+                <img src="./uploads/AI.jpg" alt="">
+                <div class="post-text">
+                    <div class="first-line">
+                        <h3 class="post-title"><a href="FullPost.php"> A Fantasy Football + Artificial Intelligence Cheat Sheet! </a></h3>
+                        <p class="read-time">6 mins</p>
+                    </div>
+                    <div class="second-line">
+                        <p class="post-descp">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Perspiciatis fugiat minima dolore nulla sequi. Architecto a eveniet omnis eligendi, amet quaerat ad maiores ipsam eius placeat maxime animi. 
+                            Repellendus blanditiis minus facere ullam, adipisci enim nostrum rerum nam facilis nobis esse placeat veniam sint delectus quasi quibusdam vero. 
+                            
+                        </p>
+                    </div>
+                    <div class="third-line">
+                        <img src="https://img.icons8.com/ios/25/000000/applause.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Right Panel -->
+        <div class="right-panel">
+            <!-- About Me -->
+            <div class="about-me">
+                <img src="https://img.icons8.com/ios-filled/150/000000/user-male-circle.png" alt="user-img">
+                <p>John Doe</p>
+            </div>
+            <!-- Categories -->
+            <div class="category-container">
+                <h4>Categories</h4>
+                <div class="category-content">
+                    <span><a href="Blog.php?Category=">BlockChain</a></span>
+                    <span><a href="Blog.php?Category=">BlockChain</a></span>
+                    <span><a href="Blog.php?Category=">BlockChain</a></span>
+                    <span><a href="Blog.php?Category=">BlockChain</a></span>
+                    <span><a href="Blog.php?Category=">BlockChain</a></span>
+                </div>
+            </div>
+            <!-- Recent Posts -->
+            <div class="recent-posts-container">
+                <h4>Recent Posts</h4>
+                <div class="recent-posts-content">
+                    <img src="./uploads/AI.jpg" alt="">
+                    <div class="recent-post">
+                        <p><a href=""> POst Title</a></p>
+                        <p id="time">8 min</p>
+                    </div>
+                </div>
+                <div class="recent-posts-content">
+                    <img src="./uploads/AI.jpg" alt="">
+                    <div class="recent-post">
+                        <p><a href=""> Fantasy Football + Artificial Intelligence Cheat Sheet!</a></p>
+                        <p id="time">8 min</p>
+                    </div>
+                </div>
+                <div class="recent-posts-content">
+                    <img src="./uploads/AI.jpg" alt="">
+                    <div class="recent-post">
+                        <p><a href=""> POst Title</a></p>
+                        <p id="time">8 min</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    
 </body>
 </html>
