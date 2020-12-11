d2619a4019291cf3841e30be1e9cf85b8ad8cc38
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-12-12 01:02:29 +0530

    :rocket: SQL database hosted

--- include/DB.php
+++ include/DB.php
@@ -1,4 +1,10 @@
 <?php 
-    $Connection = new mysqli('localhost', 'root', '');
-    $ConnectingDB = $Connection -> select_db('TechVents');
+    // Local Database
+    // $Connection = new mysqli('localhost', 'root', '');
+    // $ConnectingDB = $Connection -> select_db('TechVents');
+
+    //Deployed Database
+    $Connection = new mysqli('remotemysql.com', '8Q4BVzL1B5', '9bs2ZDV0Xe');
+    $ConnectingDB = $Connection -> select_db('8Q4BVzL1B5');
+    
 ?>
--- techvents.sql
+++ techvents.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2020 at 11:10 AM
+-- Generation Time: Dec 11, 2020 at 08:08 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
