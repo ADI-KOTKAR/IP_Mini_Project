51c7e9464622d1b82dea301d1fb22132bb490764
ADI-KOTKAR <adityakotkar75@gmail.com>
2020-12-13 20:47:11 +0530

    :package: db secured

--- include/DB.php
+++ include/DB.php
@@ -1,14 +1,5 @@
 <?php 
     // Local DB
-    $Connection = new mysqli('localhost', 'root', '');
-    $ConnectingDB = $Connection -> select_db('TechVents'); 
-
-    // Hosted DB
-    //$Connection = new mysqli('sql12.freemysqlhosting.net', 'sql12381795', 'CqQNmfwF5M');
-    // $ConnectingDB = $Connection -> select_db('sql12381795');
-
-    //AWS Hosting
-    //3S33djdfNv
-    // $Connection = new mysqli('techvents.cgath9fvd91j.us-east-2.rds.amazonaws.com', 'admin', '3S33djdfNv');
-    // $ConnectingDB = $Connection -> select_db('techvents');
+    $Connection = new mysqli('host_name', 'db_username', 'db_user_password');
+    $ConnectingDB = $Connection -> select_db('db_name'); 
 ?>
